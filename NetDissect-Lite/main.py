b5c0d65b816eeb0efdb897e46b10d5bbfbb0be01
chandana44 <chandana.amanchi@gmail.com>
2018-02-26 12:08:55 -0600

    separate bounding box class, fix for two boxes with same word, running on test images, template in multiple lines

--- NetDissect-Lite/main.py
+++ NetDissect-Lite/main.py
@@ -8,6 +8,7 @@ from map_units import *
 import numpy as np
 import os
 from PIL import Image, ImageFont, ImageDraw, ImageColor
+from bounding_box import BoundingBox
 from scipy.misc import imread, imresize, imsave
 import settings
 
@@ -59,16 +60,16 @@ def get_units_names(feature_blobs, layer):
     mapunits = MapUnits(MODEL_FOLDER, layer)
     units_map = mapunits.getunitsmap()
     names = [units_map[unit + 1]['label'] for unit in units]
-    print units, names
+    #print units, names
     return units, names
 
-def get_bounding_box(layer, units, unit_num, color, image_shape):
+def get_bounding_box(layer, units, unit_num, image_shape):
     unit = units[layer][unit_num]
     mask = imresize(features_blobs[layer][0][unit], image_shape, mode='F')
     thresholds = np.load(MODEL_FOLDER + settings.LAYERS_NAMES[layer] + '_quantile.npy')
     indexes = np.argwhere(mask > thresholds[unit])
     if indexes.size == 0:
-        return patches.Rectangle((0, 0), 0, 0, linewidth=2, edgecolor=color, facecolor='none')
+        return 0, 0, 0, 0
     else:
         min = indexes.min(0)
         x1 = min[0]
@@ -76,9 +77,10 @@ def get_bounding_box(layer, units, unit_num, color, image_shape):
         max = indexes.max(0)
         x2 = max[0]
         y2 = max[1]
-        rect = patches.Rectangle((y1, x1), y2 - y1, x2 - x1, linewidth=2, edgecolor=color, facecolor='none')
+        return x1, y1, x2, y2
+        #rect = patches.Rectangle((y1, x1), y2 - y1, x2 - x1, linewidth=2, edgecolor=color, facecolor='none')
         # ellipse = patches.Ellipse(((y1+y2)/2, ((x1+x2)/2)), y2-y1+2, x2-x1+2, linewidth=1, edgecolor= color, facecolor='none')
-        return rect
+        #return rect
 
 model = load_model(hook_feature)
 
@@ -88,18 +90,19 @@ normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 
 to_tensor = transforms.ToTensor()
 
 colors = ['red', 'blue', 'green', 'cyan']
-IMAGES_DIR = 'val_images/'
+IMAGES_DIR = 'sample_test_images/'
 if not os.path.exists(settings.OUTPUT_FOLDER):
     os.makedirs(settings.OUTPUT_FOLDER)
 
 for imagename in os.listdir(IMAGES_DIR):
+#for imagename in ['Places365_test_00000689.jpg']:
+    print imagename
     imagefile = IMAGES_DIR + imagename
     image_grounded = settings.OUTPUT_FOLDER + imagename
 
     image = Image.open(imagefile)
     image_var = Variable(normalize(to_tensor(scaler(image))).unsqueeze(0)).cuda()
     logit = model.forward(image_var)
-    print imagename
 
     units = []
     words = []
@@ -114,19 +117,29 @@ for imagename in os.listdir(IMAGES_DIR):
     fig, ax = plt.subplots(1)
 
     word_rect_map = collections.OrderedDict()
-    color_id = 0
+    color_id = -1
     answer = words[4][0]
     for layer in range(2, 4):
         for topunit in range(0, 2):
-            word_rect_map[words[layer][topunit]] = get_bounding_box(layer, units, topunit, colors[color_id], image_shape)
-            color_id += 1
+            #print words[layer][topunit]
+            if not words[layer][topunit] in word_rect_map:
+                word_rect_map[words[layer][topunit]] = []
+                color_id +=1
+                color = colors[color_id]
+            else:
+                color = word_rect_map[words[layer][topunit]][0].color
+            x1, y1, x2, y2 = get_bounding_box(layer, units, topunit, image_shape)
+            #print x1, y1, x2, y2
+            r = BoundingBox(x1, y1, x2, y2, color)
+            word_rect_map[words[layer][topunit]].append(r)
 
     # Display the image
     ax.imshow(image_array)
     # Add the patch to the Axes
-    for word in word_rect_map.keys():
-        ax.add_patch(word_rect_map[word])
-    plt.axis('off')
+    for value in word_rect_map.values():
+        for r in value:
+            ax.add_patch(patches.Rectangle((r.y1, r.x1), r.y2 - r.y1, r.x2 - r.x1, linewidth=2, edgecolor=r.color, facecolor = 'None'))
+    #plt.axis('off')
     plt.savefig(image_grounded)
     plt.close()
 
@@ -139,13 +152,33 @@ for imagename in os.listdir(IMAGES_DIR):
     font = ImageFont.truetype("fonts/SSbold.ttf", 18)
     text_w, text_h = draw.textsize("Chandana", font)
 
-    start_pixel = 100
-    draw.text((start_pixel, height - 4 * text_h), answer, ImageColor.getrgb('black'), font=font)
+    dist_from_boundary = 50
 
-    for (word, color) in zip(word_rect_map.keys(), colors):
-        draw.text((start_pixel, height - 2 * text_h), word, ImageColor.getrgb(color), font=font)
-        start_pixel += draw.textsize(word + ' ', font = font)[0]
-        print start_pixel
+    def write_text(pixel_w, line, text, color):
+        text_size = draw.textsize(text, font=font)[0]
+        if pixel_w + text_size > width-dist_from_boundary:
+            pixel_w = dist_from_boundary
+            line -= 1
+        draw.text((pixel_w, height - line * text_h), text, ImageColor.getrgb(color), font=font)
+        pixel_w += draw.textsize(text, font=font)[0]
+        return pixel_w, line
+
+
+    line_num = 3
+    start_pixel_w = dist_from_boundary
+    text = 'This is ' + answer + ' because there is '
+    start_pixel_w, line_num = write_text(start_pixel_w, line_num, text, 'black')
+
+    for word_id in range(len(word_rect_map)):
+        if word_id == len(word_rect_map)-1:
+            text = 'and '
+            start_pixel_w, line_num = write_text(start_pixel_w, line_num, text, 'black')
+            text = word_rect_map.keys()[word_id]
+        elif word_id == len(word_rect_map)-2:
+            text = word_rect_map.keys()[word_id] + ' '
+        else:
+            text = word_rect_map.keys()[word_id] + ', '
+        start_pixel_w, line_num = write_text(start_pixel_w, line_num, text, colors[word_id])
     img.save(image_grounded)
 
     features_blobs = []
